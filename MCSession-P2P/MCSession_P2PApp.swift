6b21d611018f0b079c572770a1e048db3b14da5e
권석기 <kwons3327@gmail.com>
2025-11-12 02:30:52 +0900

    Initial Commit

---
+++ MCSession-P2P/MCSession_P2PApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCSession_P2PApp.swift
+//  MCSession-P2P
+//
+//  Created by a on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MCSession_P2PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
